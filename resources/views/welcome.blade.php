74f338f76eea48777c3cdd55a591d685b5159a4c
Alan Martinson <rrtx2000-git@yahoo.com>
2017-11-07 21:12:37 -0600

    First

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>P4 - placeholder</title>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Alan Martinson P4 - placeholder
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
