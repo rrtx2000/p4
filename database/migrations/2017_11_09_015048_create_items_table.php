43d1628a59f40de8b573eacee2bb4b77898ba14b
Alan Martinson <rrtx2000-git@yahoo.com>
2017-11-08 20:20:48 -0600

    migrations added

---
+++ database/migrations/2017_11_09_015048_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            
+            $table->string('item');
+            $table->boolean('include')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('sortorder')->nullable();
+            
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
